1121c033786d5fc086a5c3e82f012c6993310810
Alison Puerta <131219053+YisethVelez000@users.noreply.github.com>
2024-03-27 13:58:25 -0500

    Corregimos la conexon

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ApiDevelopment.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //builder.Services.AddDbContext<DevelopmentContext>(p => p.UseInMemoryDatabase("DevelopmentApi")); //Nombre de la base de datos
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration.GetConnectionString("cnTareas"));
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("cnTareas")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
